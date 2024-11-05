8e23128ada75a9582efec05aa75393b748abbbcc
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-11-05 21:55:47 +0100

    Added generate dataset function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from utils.hugginface import upload_missing_files, download_missing_files
 from utils.health import health_check
+from evals.creative_factual import generate_creative_factual_dataset
 from dotenv import load_dotenv
 import typer
 
@@ -34,6 +35,27 @@ def download():
     """
 
     download_missing_files(local_dir="files", hf_subdir="files")
+
+
+@app.command()
+def generate_dataset(type: str, N: int = 50):
+    """
+    Generate a creative or factual dataset.
+
+    Args:
+        type (str): The type of dataset to generate, either 'creative' or 'factual'.
+        N (int): The number of prompts to generate. Default is 50.
+    """
+    if type not in ["creative", "factual"]:
+        typer.echo(
+            "[bold red]Invalid type. Choose either 'creative' or 'factual'[/bold red]."
+        )
+        raise typer.Exit(code=1)
+
+    generate_creative_factual_dataset(type, N)
+    typer.echo(
+        f"[bold green]{type.capitalize()} dataset with {N} prompts generated successfully.[/bold green]"
+    )
 
 
 if __name__ == "__main__":
--- evals/creative_factual.py
+++ evals/creative_factual.py
@@ -1,9 +1,38 @@
-import instructor
+from evals.creative_factual_prompts import creative_prompt, factual_prompt
+from typing import List, Literal
 from pydantic import BaseModel
 from openai import OpenAI
-from typing import List
+import instructor
+import csv
+import os
 
 
-class Prompts(BaseModel):
-    factual: List[str]
-    creative: List[str]
+class Output(BaseModel):
+    prompts: List[str]
+
+
+def generate_creative_factual_dataset(
+    type: Literal["creative", "factual"], N: int = 50
+):
+    client = instructor.from_openai(OpenAI())
+    if type == "creative":
+        prompt = creative_prompt(N)
+    else:
+        prompt = factual_prompt(N)
+
+    output = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "user", "content": prompt}],
+        response_model=Output,
+    )
+
+    output_dir = f"./files/creative_factual"
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_file = f"{output_dir}/{type}_{N}_prompts.csv"
+
+    with open(output_file, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["prompt"])
+        for prompt in output.prompts:
+            writer.writerow([prompt])
