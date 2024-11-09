d8a0ccb40503d94432e8e832a31bb94bcb312d2e
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-11-09 21:29:11 +0100

    Removed creative factual generation code -- needless

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from utils.hugginface import upload_missing_files, download_missing_files
 from utils.health import health_check
-from creative_factual.generate import generate_creative_factual_dataset
 from inference.generate import generate_data
 from dotenv import load_dotenv
 from rich import print
@@ -38,30 +37,6 @@ def download():
     """
 
     download_missing_files(local_dir="files", hf_subdir="files")
-
-
-@app.command()
-def generate_dataset(
-    type: str = Argument(..., help="The type of dataset to generate."),
-    N: int = Option(50, help="The number of prompts to generate."),
-):
-    """
-    Generate a creative or factual dataset.
-
-    Args:
-        type (str): The type of dataset to generate, either 'creative' or 'factual'.
-        N (int): The number of prompts to generate. Default is 50.
-    """
-    if type not in ["creative", "factual"]:
-        print(
-            "[bold red]Invalid type. Choose either 'creative' or 'factual'[/bold red]."
-        )
-        raise typer.Exit(code=1)
-
-    generate_creative_factual_dataset(type, N)
-    print(
-        f"[bold green]{type.capitalize()} dataset with {N} prompts generated successfully.[/bold green]"
-    )
 
 
 @app.command()
